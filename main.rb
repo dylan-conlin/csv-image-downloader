9df6d7908c141b6af271ef02e522f495794145c5
Dylan Conlin <dylan.conlin@gmail.com>
2016-03-09 14:17:30 -0800

    added progess bar gem and command-line usage
    You can now call script from the command line as follows:
        # ruby main.rb -c "/Users/dylanconlin/Dropbox/shortstack-user-images/share-your-story.csv" -d "/Users/dylanconlin/Dropbox/shortstack-user-images/"

--- main.rb
+++ main.rb
@@ -1,36 +1,77 @@
 require 'csv'
 require 'open-uri'
+require 'optparse'
+require 'yaml'
+require 'ruby-progressbar'
+
+# example command
+# ruby main.rb -c "/Users/dylanconlin/Dropbox/shortstack-user-images/share-your-story.csv" -d "/Users/dylanconlin/Dropbox/shortstack-user-images/"
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+  opts.on('-c', '--csv_path PATH', 'CSV path') { |v| options[:csv_path] = v }
+  opts.on('-d', '--image_directory DIRECTORY', 'Image directory path') { |v| options[:image_directory] = v }
+end.parse!
+
+errors = []
+
+unless (options[:csv_path] && options[:csv_path].length > 0)
+  errors << 'Error: `-c csv_path` is required'
+end
+
+unless (options[:image_directory] && options[:image_directory].length > 0)
+  errors << 'Error: `-c image_directory` is required'
+end
+
+if errors.length > 0
+  puts errors
+  return 
+end
 
 # directory where you want to store the images
-images_dir = "/Users/dylanconlin/Downloads/shortstack-user-images/"
+images_dir = options[:image_directory]
 
 # location of the csv you're reading
-csv_path = "/Users/dylanconlin/Downloads/photo-thing.csv"
+csv_path = options[:csv_path]
 
 # column number of image url (zero-based)
-image_url_row = 16
+image_column = nil
 
 # column number of entry_id (zero-based). This is used to image labeling.
-entry_id_row = 25
+entry_id_column = nil
 
 images = []
 
 CSV.foreach(csv_path) do |row|
-  images.push({ :entry_id => row[entry_id_row], :url => row[image_url_row] })
+  entry_id_column = row.index('entry_id')
+  image_column = row.index('image') || row.index('image_url')
+  break unless (entry_id_column.nil? && image_column.nil?)
+end
+
+CSV.foreach(csv_path) do |row|
+  images.push({ :entry_id => row[entry_id_column], :url => row[image_column] })
 end
 
 images = images.drop(1)
 
+progressbar = ProgressBar.create(:title => "Images", :starting_at => 0, :total => images.length, :format => '%a |%B| (%c/%C) %t')
+
 images.each do |image|
   entry_id = image[:entry_id]
   url = image[:url]
-  
   unless url.nil?
     filename = "#{images_dir}entry_#{entry_id}_#{File.basename(url)}"
     open(filename, 'wb') do |file|
-      puts "writing #{url} to #{filename}"
-      file << open(url).read
+      if url and url.length > 0
+        file << open(url).read
+      else
+        file << "This entry did not include a url."
+      end
+      progressbar.increment
     end
   end
+  
 end
 
