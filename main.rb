c8dae9f32264f9f049ebd1d7aa42e08265d42d9c
Dylan Conlin <dylan.conlin@gmail.com>
2016-04-14 09:52:57 -0700

    added hacky fix to allow download to resume

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@ require 'optparse'
 require 'yaml'
 require 'ruby-progressbar'
 require 'fileutils'
+require 'faraday'
+require 'awesome_print'
 
 # This script takes 1 argument: the full path name of the
 # csv file containing the images you'd like to export.
@@ -33,52 +35,117 @@ csv_path = options[:csv_path]
 
 images_dir_name = File.basename(csv_path, File.extname(csv_path))
 
-Dir.mkdir(images_dir_name)
+if File.directory?(images_dir_name)
+  last_file = Dir["#{images_dir_name}/*"].sort_by { |f| File.basename(f)[/\d+/].to_i }[-1]
+  last_file_number = File.basename(last_file)[/\d+/].to_i
 
-FileUtils.cp( csv_path, "#{images_dir_name}/#{File.basename(csv_path)}" )
+  # column number of image url (zero-based)
+  image_column = nil
 
-# column number of image url (zero-based)
-image_column = nil
+  # column number of entry_id (zero-based). This is used to image labeling.
+  entry_id_column = nil
 
-# column number of entry_id (zero-based). This is used to image labeling.
-entry_id_column = nil
+  images = []
 
-images = []
-
-# Find the entry_id and image url columns (used when naming images).
-CSV.foreach(csv_path) do |row|
-  entry_id_column = row.index('entry_id')
-  image_column = row.index('image') || row.index('image_url')
-  break unless (entry_id_column.nil? && image_column.nil?)
-end
-
-CSV.foreach(csv_path) do |row|
-  images.push({ entry_id: row[entry_id_column], url: row[image_column] })
-end
-
-images = images.drop(1)
-
-progressbar = ProgressBar.create(
-  title: "Images",
-  starting_at: 0,
-  total: images.length,
-  format: '%a |%B| (%c/%C) %t'
-)
-
-images.each do |image|
-  entry_id = image[:entry_id]
-  url = image[:url]
-  unless url.nil?
-    filename = "#{images_dir_name}/entry_#{entry_id}_#{File.basename(url)}"
-    open(filename, 'wb') do |file|
-      if url and url.length > 0
-        file << open(url).read
-      else
-        file << "This entry did not include a url."
-      end
-      progressbar.increment
-    end
+  # Find the entry_id and image url columns (used when naming images).
+  CSV.foreach(csv_path) do |row|
+    entry_id_column = row.index('entry_id')
+    image_column = row.index('image') || row.index('image_url')
+    break unless (entry_id_column.nil? && image_column.nil?)
   end
   
-end
+  img_count = -1 # to account for the headers
+  
+  CSV.foreach(csv_path) do |row|
+    img_count += 1
+    images.push({ id: img_count, entry_id: row[entry_id_column], url: row[image_column] })
+  end
+
+  total_images = images.drop(1)
+  
+  remaining_images = total_images.drop(last_file_number + 1)
+  
+  progressbar = ProgressBar.create(
+    title: "Images",
+    starting_at: last_file_number,
+    total: total_images.length,
+    format: '%a |%B| (%c/%C) %t'
+  )
+
+  image_count = last_file_number
+  
+  remaining_images.each do |image|
+    entry_id = image[:entry_id]
+    url = image[:url]
+    
+    if Faraday.head(url).status == 200 && !url.nil?
+      image_count += 1
+      filename = "#{images_dir_name}/#{image_count}_entry_#{entry_id}_#{File.basename(url)}"
+      open(filename, 'wb') do |file|
+        if url and url.length > 0
+          file << open(url).read
+        else
+          file << "This entry did not include a url."
+        end
+        progressbar.increment
+      end
+    end
+    
+  end
+
+  
+else
+  Dir.mkdir(images_dir_name)
+  FileUtils.cp(csv_path, "#{images_dir_name}/#{File.basename(csv_path)}")
+
+  # column number of image url (zero-based)
+  image_column = nil
+
+  # column number of entry_id (zero-based). This is used to image labeling.
+  entry_id_column = nil
+
+  images = []
+
+  # Find the entry_id and image url columns (used when naming images).
+  CSV.foreach(csv_path) do |row|
+    entry_id_column = row.index('entry_id')
+    image_column = row.index('image') || row.index('image_url')
+    break unless (entry_id_column.nil? && image_column.nil?)
+  end
+
+  CSV.foreach(csv_path) do |row|
+    images.push({ entry_id: row[entry_id_column], url: row[image_column] })
+  end
+
+  images = images.drop(1)
+
+  progressbar = ProgressBar.create(
+    title: "Images",
+    starting_at: 0,
+    total: images.length,
+    format: '%a |%B| (%c/%C) %t'
+  )
+
+  image_count = 0
+
+  images.each do |image|
+    entry_id = image[:entry_id]
+    url = image[:url]
+    
+    if Faraday.head(url).status == 200 && !url.nil?
+      image_count += 1
+      filename = "#{images_dir_name}/#{image_count}_entry_#{entry_id}_#{File.basename(url)}"
+      open(filename, 'wb') do |file|
+        if url and url.length > 0
+          file << open(url).read
+        else
+          file << "This entry did not include a url."
+        end
+        progressbar.increment
+      end
+    end
+    
+  end
+
+end  
 
